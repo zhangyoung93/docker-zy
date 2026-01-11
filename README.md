ab7d003570986e1ef94370752a5e0d76b98dbf52
zhangyoung93 <68599345+zhangyoung93@users.noreply.github.com>
2026-01-11 20:04:58 +0800

    build(redis): add redis sentinel demo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# docker-zy
+# 后台启动：docker compose up -d
+
+# 后台启动指定服务：docker compose up -d [service-name]
+
+# 重建镜像后启动：docker compose up -d --build
+
+# 停止但保留容器：docker compose stop
+
+# 停止但保留指定容器：docker compose stop [service-name]
+
+# 停止并删除容器，重建镜像用：docker compose down
--- database/redis/bin/redis-start.sh
+++ database/redis/bin/redis-start.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
-
-#设置脚本执行过程中发生错误时立即终止运行
-set -e
-
-#解析变量
-exec redis-server ${REDIS_HOME}/redis.conf --requirepass ${REDIS_PASSWORD}
+##!/bin/sh
+#
+##设置脚本执行过程中发生错误时立即终止运行
+#set -e
+#
+##redis启动辅助命令
+#redis_server_cmd=${REDIS_HOME}/redis.conf --requirepass ${REDIS_PASSWORD}
+#
+##MASTER节点信息不为空则拼接命令
+#if [-n ${REDIS_MASTER_NODE}]
+#then
+#  redis_server_cmd=" --replicaof "${REDIS_MASTER_NODE}
+#
+##解析变量
+#exec redis-server ${redis_server_cmd}
